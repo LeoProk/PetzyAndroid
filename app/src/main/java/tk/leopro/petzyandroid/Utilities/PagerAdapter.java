213e34d1746b4331e77611c9a45cfce14b6f8472
Leo <just_beta@hotmail.com>
2015-07-16 19:42:21 +0300

    added tab handaing

--- app/src/main/java/tk/leopro/petzyandroid/Utilities/PagerAdapter.java
+++ app/src/main/java/tk/leopro/petzyandroid/Utilities/PagerAdapter.java
@@ -1,30 +1,40 @@
 package tk.leopro.petzyandroid.Utilities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import tk.leopro.petzyandroid.AppController;
 
-import java.util.List;
-
-/**
+ /**
  * Created by Leo on 7/15/2015.
  */
-public class PagerAdapter extends FragmentPagerAdapter {
+final class PagerAdapter extends FragmentPagerAdapter {
 
     private String mTabTitles[];
     private Context mContext;
+    private String[] mTags;
     private Fragment[] mFragmentForTabs;
 
-    public PagerAdapter(FragmentManager fm, Context context ,String[] tabTitles) {
+    public PagerAdapter(FragmentManager fm, Context context ,String[] tabTitles,Fragment[] fragmentsforTabs,String[] tags) {
         super(fm);
         mTabTitles = tabTitles;
+        mTags = tags;
+        mFragmentForTabs = fragmentsforTabs;
         mContext = context;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return PageFragment.newInstance(position + 1);
+        final AppCompatActivity activity = ( AppCompatActivity) mContext;
+        if(activity.getFragmentManager().findFragmentByTag(mTags[position+1])==null){
+            AppController.mFragmentTag = mTags[position+1];
+            UtilitiesFactory.addFragment(mContext,mFragmentForTabs[position+1],mTags[position+1],true).doTask();
+        }else {
+            UtilitiesFactory.switchFragments(mContext,mTags[position+1]).doTask();
+        }
+        return null;
     }
 
     @Override
@@ -35,6 +45,6 @@ public class PagerAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
         // Generate title based on item position
-        return tabTitles[position];
+        return mTabTitles[position];
     }
 }
