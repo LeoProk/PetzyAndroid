e285318cbfa7d522557b743c57aefe7e20987161
Leonid Prokopenko <just_beta@hotmail.com>
2015-10-10 19:19:37 +0300

    Done some work

---
+++ app/src/main/java/tk/leopro/petzyandroid/AppSpecific/ParkMapMaker.java
@@ -0,0 +1,44 @@
+package tk.leopro.petzyandroid.AppSpecific;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tk.leopro.petzyandroid.Interfaces.FactoryInterface;
+import tk.leopro.petzyandroid.Utilities.UtilitiesFactory;
+
+/**
+ * Created by Leo on 10/10/2015.
+ */
+final class ParkMapMaker implements FactoryInterface {
+
+    private GoogleMap mMap;
+
+    private Context mContext;
+
+    public ParkMapMaker(GoogleMap map,Context context){
+
+     mContext = context;
+     mMap = map;
+
+    }
+
+    @Override
+    public Object doTask() {
+        List<Park> parksList = new ArrayList();
+        UtilitiesFactory.callSQL(mContext, parksList, "retrieve").doTask();
+        for (int i = 0; i < parksList.size() ; i++) {
+            mMap.addMarker(new MarkerOptions()
+                    .position(parksList.get(i).getLatLng())
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        }
+
+        return null;
+    }
+}
